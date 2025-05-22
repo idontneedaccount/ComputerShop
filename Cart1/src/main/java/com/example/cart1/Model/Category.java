6fa49d4b5e54a9d3365995809f8a10ca5a2399f2
Duong Van Hoang Manh <83123630+manhzx35@users.noreply.github.com>
2025-05-22 23:39:06 +0700

    Add files via upload

---
+++ Cart1/src/main/java/com/example/cart1/Model/Category.java
@@ -0,0 +1,27 @@
+package com.example.cart1.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+
+
+    @OneToMany(mappedBy = "category")
+    public List<Product> products;
+}
+
