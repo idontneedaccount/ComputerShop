5977b1d5c72442b8f3fb739a61616d1d8af1eabc
idontneedaccount <nn0469972@gmail.com>
2025-05-30 00:01:35 +0700

    sửa login them css

--- src/main/java/com/example/computershop/config/SecurityConfig.java
+++ src/main/java/com/example/computershop/config/SecurityConfig.java
@@ -6,40 +6,44 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-
-import java.util.List;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    private final AuthenticationProvider authenticationProvider;
+        private final AuthenticationProvider authenticationProvider;
+        private final AuthenticationSuccessHandler successHandler;
 
-    public SecurityConfig(AuthenticationProvider authenticationProvider) {
-        this.authenticationProvider = authenticationProvider;
-    }
+        public SecurityConfig(AuthenticationProvider authenticationProvider, AuthenticationSuccessHandler successHandler) {
+                this.authenticationProvider = authenticationProvider;
+                this.successHandler = successHandler;
+        }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(
-                        authorize -> authorize
-                                .requestMatchers("/auth/**", "/home", "/css/**", "/js/**", "/images/**").permitAll()
-                                .requestMatchers("/admin/**").permitAll()
-                                .requestMatchers("/user/**").permitAll()
-                                .anyRequest().authenticated())
-                .formLogin(form -> form
-                        .loginPage("/auth/login")
-                        .defaultSuccessUrl("/home")
-                        .permitAll())
-                .logout(logout -> logout
-                        .logoutUrl("/logout")
-                        .logoutSuccessUrl("/auth/login")
-                        .permitAll())
-                .authenticationProvider(authenticationProvider);
-        return http.build();
-    }
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+                http
+                                .csrf(csrf -> csrf.disable())
+                                .authorizeHttpRequests(
+                                                authorize -> authorize
+                                                                .requestMatchers("/auth/**", "/home", "/css/**",
+                                                                                "/js/**", "/images/**", "/assets/**")
+                                                                .permitAll()
+                                                                .requestMatchers("/admin/**").hasRole("ADMIN")
+                                                                .requestMatchers("/user/**").hasRole("USER")
+                                                                .anyRequest().authenticated())
+                                .formLogin(form -> form
+                                                .loginPage("/auth/login")
+                                                .loginProcessingUrl("/auth/login-process")
+                                                .successHandler(successHandler)
+                                                .failureUrl("/auth/login?error=true")
+                                                .permitAll())
+                                .logout(logout -> logout
+                                                .logoutUrl("/logout")
+                                                .logoutSuccessUrl("/auth/login")
+                                                .permitAll())
+                                .authenticationProvider(authenticationProvider);
+                return http.build();
+        }
+
 
 }
