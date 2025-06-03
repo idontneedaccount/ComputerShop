8e6fb8bb325a113d27a955a42079ded9edfe2230
huongquynh <truong7aka@gmail.com>
2025-06-03 16:00:40 +0700

    Toi uu file, loai bo file du thua khong can thiet

--- src/main/java/com/example/computershop/service/CategoriesService.java
+++ src/main/java/com/example/computershop/service/CategoriesService.java
@@ -1,15 +1,55 @@
 package com.example.computershop.service;
 
 import java.util.List;
-
 import com.example.computershop.entity.Categories;
+import com.example.computershop.repository.CategoriesRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
-public interface  CategoriesService {
-    List<Categories> getAll();
-    Boolean create(Categories categories);
-    Categories findById(String categoryID);
-    Boolean update(Categories categories);
-    Boolean delete(String categoryID);
-    boolean existsByName(String name);
+@Service
+@AllArgsConstructor
+public class CategoriesService {
+    private final CategoriesRepository repo;
 
+    public List<Categories> getAll() {
+        return this.repo.findAll();
+    }
+
+    public Boolean create(Categories categories) {
+        try {
+            this.repo.save(categories);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public Categories findById(String categoryID) {
+        return this.repo.findById(categoryID).orElse(null);
+    }
+
+    public Boolean update(Categories categories) {
+        try {
+            this.repo.save(categories);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public Boolean delete(String categoryID) {
+        try {
+            this.repo.deleteById(categoryID);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean existsByName(String name) {
+        return repo.existsByName(name);
+    }
 }
