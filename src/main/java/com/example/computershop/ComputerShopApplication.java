c1af93eacd0ba4566389f359f47b5be3f679f7f3
idontneedaccount <nn0469972@gmail.com>
2025-07-23 09:37:50 +0700

    translate cart review and footer content to Vietnamese

--- src/main/java/com/example/computershop/ComputerShopApplication.java
+++ src/main/java/com/example/computershop/ComputerShopApplication.java
@@ -1,16 +1,7 @@
 package com.example.computershop;
 
-import com.example.computershop.enums.Role;
-import com.example.computershop.entity.User;
-import com.example.computershop.repository.UserRepository;
-import com.example.computershop.service.StorageService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class ComputerShopApplication {
